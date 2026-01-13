45b9c5369f0c34898d76bee13cb36d616e7f2b37
Stephen Schwarz <steve@steveandkureen.com>
2026-01-12 22:14:17 -0700

    inital commit

---
+++ include/FreeRTOSConfig.h
@@ -0,0 +1,146 @@
+/*
+ * FreeRTOS Kernel V10.6.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * FreeRTOS configuration for SprinklerMaster on Raspberry Pi Pico 2 W (RP2350)
+ */
+
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+/* Hardware and basic kernel configuration */
+#define configUSE_PREEMPTION                    1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
+#define configUSE_TICKLESS_IDLE                 0
+#define configCPU_CLOCK_HZ                      ((unsigned long) 150000000)  /* RP2350 ARM at 150MHz */
+#define configTICK_RATE_HZ                      ((TickType_t) 1000)          /* 1ms tick for precise timing */
+#define configMAX_PRIORITIES                    8          /* Enough for High/Medium/Low priority tasks */
+#define configMINIMAL_STACK_SIZE                256        /* Minimum stack size in words */
+#define configMAX_TASK_NAME_LEN                 16
+#define configUSE_16_BIT_TICKS                  0
+#define configIDLE_SHOULD_YIELD                 1
+#define configUSE_TASK_NOTIFICATIONS            1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   1
+#define configUSE_MUTEXES                       1
+#define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_COUNTING_SEMAPHORES           1
+#define configQUEUE_REGISTRY_SIZE               10
+#define configUSE_QUEUE_SETS                    0
+#define configUSE_TIME_SLICING                  1
+#define configUSE_NEWLIB_REENTRANT              0
+#define configENABLE_BACKWARD_COMPATIBILITY     0
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS 0
+#define configUSE_MINI_LIST_ITEM                1
+#define configSTACK_DEPTH_TYPE                  uint32_t
+#define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
+#define configHEAP_CLEAR_MEMORY_ON_FREE         1
+
+/* Memory allocation */
+#define configSUPPORT_STATIC_ALLOCATION         0
+#define configSUPPORT_DYNAMIC_ALLOCATION        1
+#define configTOTAL_HEAP_SIZE                   (64 * 1024)   /* 64KB heap for tasks */
+#define configAPPLICATION_ALLOCATED_HEAP        0
+
+/* Hook function configuration */
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configCHECK_FOR_STACK_OVERFLOW          2  /* Enable stack overflow checking */
+#define configUSE_MALLOC_FAILED_HOOK            1
+#define configUSE_DAEMON_TASK_STARTUP_HOOK      0
+
+/* Co-routine configuration (not used) */
+#define configUSE_CO_ROUTINES                   0
+#define configMAX_CO_ROUTINE_PRIORITIES         1
+
+/* Software timer configuration */
+#define configUSE_TIMERS                        1
+#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1)
+#define configTIMER_QUEUE_LENGTH                10
+#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
+
+/* Event group configuration */
+#define configUSE_EVENT_GROUPS                  1
+
+/* Stream buffer configuration */
+#define configUSE_STREAM_BUFFERS                1
+
+/* Interrupt nesting behavior configuration */
+#define configMAX_API_CALL_INTERRUPT_PRIORITY   (0x20)
+
+/* Optional functions */
+#define INCLUDE_vTaskPrioritySet                1
+#define INCLUDE_uxTaskPriorityGet               1
+#define INCLUDE_vTaskDelete                     1
+#define INCLUDE_vTaskCleanUpResources           0
+#define INCLUDE_vTaskSuspend                    1
+#define INCLUDE_vTaskDelayUntil                 1
+#define INCLUDE_vTaskDelay                      1
+#define INCLUDE_xTaskGetSchedulerState          1
+#define INCLUDE_xTaskGetCurrentTaskHandle       1
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
+#define INCLUDE_xTaskGetIdleTaskHandle          0
+#define INCLUDE_eTaskGetState                   1
+#define INCLUDE_xTimerPendFunctionCall          1
+#define INCLUDE_xTaskAbortDelay                 1
+#define INCLUDE_xTaskGetHandle                  1
+#define INCLUDE_xTaskResumeFromISR              1
+#define INCLUDE_xSemaphoreGetMutexHolder        1
+
+/* RP2040/RP2350 specific configuration */
+#define configSUPPORT_PICO_SYNC_INTEROP         1
+#define configSUPPORT_PICO_TIME_INTEROP         1
+
+/* RISC-V specific - Machine Timer addresses in SIO */
+#define configMTIME_BASE_ADDRESS                (0xd00001b0UL)
+#define configMTIMECMP_BASE_ADDRESS             (0xd00001b8UL)
+
+/* Cortex-M specific definitions */
+#ifdef __NVIC_PRIO_BITS
+    /* __NVIC_PRIO_BITS will be specified when CMSIS is being used */
+    #define configPRIO_BITS                     __NVIC_PRIO_BITS
+#else
+    #define configPRIO_BITS                     4  /* RP2350 has 4 priority bits */
+#endif
+
+/* The lowest interrupt priority that can be used in a call to a "set priority" function */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY         0x0F
+
+/* The highest interrupt priority that can be used by any interrupt service
+ * routine that makes calls to interrupt safe FreeRTOS API functions. DO NOT
+ * CALL INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A
+ * HIGHER PRIORITY THAN THIS! (higher priorities are lower numeric values) */
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY    0x02
+
+/* Interrupt priorities used by the kernel port layer itself */
+#define configKERNEL_INTERRUPT_PRIORITY         (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+
+/* Assert configuration */
+#include <assert.h>
+#define configASSERT(x)                         assert(x)
+
+/* Compatibility for older lwIP FreeRTOS port */
+#define portTICK_RATE_MS                        portTICK_PERIOD_MS
+
+/* SMP port only */
+#define configNUMBER_OF_CORES                   2
+#define configRUN_MULTIPLE_PRIORITIES           1
+#if configNUMBER_OF_CORES > 1
+#define configUSE_CORE_AFFINITY                 1
+#endif
+#define configUSE_PASSIVE_IDLE_HOOK             0
+
+/* RP2040 specific */
+#define configSUPPORT_PICO_SYNC_INTEROP         1
+#define configSUPPORT_PICO_TIME_INTEROP         1
+
+/* RP2350 grows some features */
+#define configENABLE_FPU                        1
+#define configENABLE_MPU                        0
+#define configENABLE_TRUSTZONE                  0
+#define configRUN_FREERTOS_SECURE_ONLY          1
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    16
+
+#endif /* FREERTOS_CONFIG_H */
